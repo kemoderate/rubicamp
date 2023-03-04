0defb725dc1395589ba7cf4c081497272a9ecb93
fahmi <fahminomana@gmail.com>
2023-03-04 09:20:27 +0700

    challenge5

---
+++ c5/ayam.js
@@ -0,0 +1,19 @@
+function stringmanipulation(n){ 
+let hasilakhir = ''
+let vokal = [a,i,u,e,o]
+let konsonal = [b, c, d, f, 
+                g, h, j, k, l, m, n, 
+                p, q, r, s, t, v, w, x, y, z]
+
+
+        console.log(n)
+       
+
+    }
+
+stringmanipulation('ayam');
+stringmanipulation('bebek');
+
+
+
+
