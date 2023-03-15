5e3be483201affc07d33682a818fb47ae91f618d
fahmi <fahminomana@gmail.com>
2023-03-15 20:16:23 +0700

    challenge9selesai

--- c9/spiral.js
+++ c9/spiral.js
@@ -1,60 +1,57 @@
 function spiral(size) {
-  const matriks = []
-  let hitung = 0
+  const matriks = [];
+  let hitung = 0;
 
   for (let i = 0; i < size; i++) {
-      matriks[i] = []
-      for (let j = 0; j < size; j++) {
-          matriks[i][j] = hitung
-          hitung++
-      }
-
+    matriks[i] = []
+    for (let j = 0; j < size; j++) {
+      matriks[i][j] = hitung
+      hitung++
+    }
   }
- 
-  let x = 0
-  let y = 0
-  let batasAtas = size
-  let batasBawah = 0
-  let result = []
-  while (result.length < size * size) {
 
+    let x = 0;
+    let y = 0;
+    let batasAtas = size;
+    let batasBawah = 0;
+    let result = [];
 
+    while (result.length < size * size) {
       // ke kanan
-      for (; x < batasAtas; x++) {
-          // console.log(y,x)
-          result.push(matriks[y][x])
+      while (x < batasAtas) {
+        result.push(matriks[y][x]);
+        x++;
       }
-      x--
-      y++
+      x--;
+      y++;
       // ke bawah
-
-      for (; y < batasAtas; y++) {
-          // console.log(y,x)
-          result.push(matriks[y][x])
+      while (y < batasAtas) {
+        result.push(matriks[y][x]);
+        y++;
       }
       // ke kiri
-      y--
-      x--
-      for (; x >= batasBawah; x--) {
-          // console.log(y,x)
-          result.push(matriks[y][x])
-
+      y--;
+      x--;
+      while (x >= batasBawah) {
+        result.push(matriks[y][x]);
+        x--;
       }
       // ke atas
-      x++
-      y--
-      for (; y > batasBawah; y--) {
-          // console.log(y,x)
-          result.push(matriks[y][x])
+      x++;
+      y--;
+      while (y > batasBawah) {
+        result.push(matriks[y][x]);
+        y--;
       }
-      x++
-      y++
-      batasAtas--
-      batasBawah++
+      x++;
+      y++;
+      batasAtas--;
+      batasBawah++;
+    }
 
+    console.log(result);
   }
-  console.log(result)
-}
-spiral(5)
-// spiral(6)
-// spiral(7)
+
+  spiral(5);
+  spiral(6);
+  spiral(7);
