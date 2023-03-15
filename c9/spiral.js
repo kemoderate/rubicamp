27f1674a63d10cd5de28b1c0174e8e164f4deec0
fahmi <fahminomana@gmail.com>
2023-03-15 19:43:10 +0700

    challenge9selesai

--- c9/spiral.js
+++ c9/spiral.js
@@ -1,29 +1,60 @@
-function spiralMatrix(n) {
-    let result = []
-    let hitung = 0;
+function spiral(size) {
+  const matriks = []
+  let hitung = 0
 
-    for (let i = 0; i < n; i++) {
-      result[i] = [];
-      // console.log(result)
-      for (let j = 0; j < n; j++) {
-        result[i][j] = hitung;
-        hitung++
+  for (let i = 0; i < size; i++) {
+      matriks[i] = []
+      for (let j = 0; j < size; j++) {
+          matriks[i][j] = hitung
+          hitung++
       }
-    }
-    console.log(result)
-    //  console.log(hitung)
 
-     console.log(result[3][0])
-
-     let x = result[0]
-     let y = result[4]
-    
-  
-
-
-     
-    
   }
-  spiralMatrix(5);
-  
-  
+ 
+  let x = 0
+  let y = 0
+  let batasAtas = size
+  let batasBawah = 0
+  let result = []
+  while (result.length < size * size) {
+
+
+      // ke kanan
+      for (; x < batasAtas; x++) {
+          // console.log(y,x)
+          result.push(matriks[y][x])
+      }
+      x--
+      y++
+      // ke bawah
+
+      for (; y < batasAtas; y++) {
+          // console.log(y,x)
+          result.push(matriks[y][x])
+      }
+      // ke kiri
+      y--
+      x--
+      for (; x >= batasBawah; x--) {
+          // console.log(y,x)
+          result.push(matriks[y][x])
+
+      }
+      // ke atas
+      x++
+      y--
+      for (; y > batasBawah; y--) {
+          // console.log(y,x)
+          result.push(matriks[y][x])
+      }
+      x++
+      y++
+      batasAtas--
+      batasBawah++
+
+  }
+  console.log(result)
+}
+spiral(5)
+// spiral(6)
+// spiral(7)
