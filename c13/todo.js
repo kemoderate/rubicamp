4127788ab32bbe4ec64db5c2895b14da2bb71287
fahmi <fahminomana@gmail.com>
2023-03-23 13:03:43 +0700

    challenge13gantiswitch

--- c13/todo.js
+++ c13/todo.js
@@ -1,71 +1,140 @@
-const readline = require('readline');
 const fs = require('fs');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  
- 
+const process = require('process');
+const { json } = require('stream/consumers');
+
+
+
 const data = fs.readFileSync('tasks.json')
 const taskData = JSON.parse(data);
 
+//function 
+function addTask() {
+  taskData.push({
+    "task_id": null,
+    "task_content": content,
+    "status": false,
+    "tag": []
 
-function addTask(){
-    taskData.push(args.slice(1).join(" "))
-   fs.writeFileSync('tasks.json',JSON.stringify(taskData),"utf-8")
+
+  })
+  fs.writeFileSync('tasks.json', JSON.stringify(taskData, null, 4));
+
 }
-    
-function deleteTask(){
- taskData.splice()
+
+function deleteTask() {
+  let indexDelete = parseInt(args1) - 1;
+  // console.log(indexDelete);
+  let dataItem = taskData[indexDelete];
+  taskData.splice(indexDelete, 1);
+  fs.writeFileSync('tasks.json', JSON.stringify(taskData, null, 4));
+  console.log(`"${dataItem.task_content}" telah di hapus dari daftar`);
 }
 
-const args = process.argv.slice(2); 
+function showList() {
+  for (let i = 0; i < taskData.length; i++) {
+    console.log(`${i + 1}.[${taskData[i].status ? 'X' : ' '}] ${taskData[i].task_content}`);
+  }
+}
+function showTask(){
+  for(let i = 0; i <taskData.length; i++)
+    console.log(`task_id : ${i+1}.[${taskData[i].status ? 'X': ' '}] ${taskData[i].task_content}`);
+  }
 
 
-const command = args[0]; 
+function completeTask() {
+  let taskComplete = parseInt(args1) - 1;
+  let dataItem = taskData[taskComplete]
+  let complete = taskData[taskComplete].status = true
+  fs.writeFileSync('tasks.json', JSON.stringify(taskData, null, 4));
+  console.log(`"${dataItem.task_content}" telah selesai`);
+}
+function uncompleteTask() {
+  let taskComplete = parseInt(args1) - 1;
+  let dataItem = taskData[taskComplete]
+  let complete = taskData[taskComplete].status = false
+  fs.writeFileSync('tasks.json', JSON.stringify(taskData, null, 4));
+  console.log(`"${dataItem.task_content}" status selesai telah dibatalkan.`);
+}
+
+
+
+const args = process.argv.slice(2);
+const command = args[0];
+const args1 = process.argv[3]
+let content = args.slice(1).join(" ")
+const commandList = ` >>> JS TODO <<< 
+ $node todo.js <command>
+ $node todo.js list
+ $node todo.js task <task_id>
+ $node todo.js add <task_content>
+ $node todo.js delete <task_id>
+ $node todo.js complete <task_id>
+ $node todo.js uncomplete <task_id>
+ $node todo.js list:outstanding asc|desc
+ $node todo.js list:completed asc|desc
+ $node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>");
+ $node todo.js filter:<tag_name>`
 
 switch (command) {
+  case "help":
+    console.log(commandList)
+    break;
   case "list":
-    console.log("todo.js list");
+    console.log("Daftar Pekerjaan");
+    showList();
     break;
   case "task":
-    console.log(`todo.js task ${args[1]}`);
-    break;
-  case "add": 
-   addTask()
-    console.log(`"${args.slice(1).join(" ")}" telah di tambahkan. `);
+  showTask();
+      break;
+  case "add":
+    addTask();
+    console.log(`"${content}" telah di tambahkan. `);
     break;
   case "delete":
     deleteTask();
-    console.log(`todo.js delete ${args[1]}`);
+    // console.log(`"${dataItem.task_content}" telah di hapus dari daftar`);
     break;
   case "complete":
-    console.log(`todo.js complete ${args[1]}`);
+    completeTask();
     break;
   case "uncomplete":
-    console.log(`todo.js uncomplete ${args[1]}`);
+    uncompleteTask();
     break;
-  case "list:outstanding":
-    console.log(`todo.js list:outstanding ${args[1]}`);
-    break;
+    case "list:outstanding":
+      switch(args1) {
+        case "asc":
+          console.log("Daftar Pekerjaan");
+          for (let i = 0; i < taskData.length; i++) {
+            if (taskData[i].status === false) {
+              console.log(`${i + 1}.[ ]${taskData[i].task_content}`);
+            }
+          }break;
+        case "desc":
+          console.log("Daftar Pekerjaan");
+          for (let j = taskData.length - 1; j >= 0; j--) {
+            if (taskData[j].status === false) {
+              console.log(`${j+1}.[ ]${taskData[j].task_content}`);
+            }
+          }
+          break;
+      }break;
   case "list:completed":
-    console.log(`todo.js list:completed ${args[1]}`);
+    
     break;
   case "tag":
-    console.log(`todo.js tag ${args[1]} ${args.slice(2).join(" ")}`);
+    let indexTags = parseInt(args2) - 1;
+    let words = '';
+    let tags = taskData[indexTags].tag;
+    let tag = taskData[indexTags];
+    for (let i = 4; i < args.length; i++) {
+      tags.push(args[i]);
+      console.log(args[i])
+      words += args[i] + ' ';
+    }   
+    fs.writeFileSync('data.json', JSON.stringify(taskData, null, 4));
+    console.log(`Tags '${words.trim()}' telah ditambahkan ke daftar ${tag.task_content}`);
     break;
   default:
-    console.log(" >>> JS TODO <<< ");
-    console.log("todo.js <command>");
-    console.log("todo.js list");
-    console.log("todo.js task <task_id>");
-    console.log("todo.js add <task_content>");
-    console.log("todo.js delete <task_id>");
-    console.log("todo.js complete <task_id>");
-    console.log("todo.js uncomplete <task_id>");
-    console.log("todo.js list:outstanding asc|desc");
-    console.log("todo.js list:completed asc|desc");
-    console.log("todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>");
-    console.log("todo.js filter:<tag_name>");
+    console.log(commandList)
     break;
 }
