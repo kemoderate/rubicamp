4dc388b3ad1b53caf000190f93ea1e23e648f786
fahmi <fahminomana@gmail.com>
2023-05-16 11:00:18 +0700

    challange17try1

---
+++ c17/calculator.js
@@ -0,0 +1,44 @@
+const PI = 22 / 7
+class Calculator {
+    constructor() {
+        this.resultValue = 1;
+
+}
+
+add(value) {
+    this.resultValue += value;
+    return this;
+
+}
+multiply(value){
+    this.resultValue *= value;
+    return this;
+}
+substract(value){
+    this.resultValue -= value;
+    return this;
+}
+divide(value){
+    this.resultValue /= value;
+    return this;
+}
+square(value){  
+    this.resultValue = Math.pow(value,2);
+    return this;
+}
+exponent(value){
+    this.resultValue = Math.pow(this.resultValue,value);
+    return this;
+}
+squareRoot(value){
+    this.resultValue = Math.sqrt(value);
+    return this;
+}
+
+// tambahkan method lain yang perlu
+result(){
+return this.resultValue;
+}
+}
+const calc = new Calculator
+module.exports = Calculator,PI;
