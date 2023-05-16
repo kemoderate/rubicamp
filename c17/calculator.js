7522e7bd3dc281c296694af6490c5cf218452cc2
fahmi <fahminomana@gmail.com>
2023-05-16 14:19:10 +0700

    challenge17selesai

--- c17/calculator.js
+++ c17/calculator.js
@@ -1,45 +1,45 @@
 const PI = 22 / 7
 class Calculator {
     constructor() {
-        this.resultValue = 1;
+        this.x = 1;
 
+    }
+
+    add(value) {
+        this.x += value;
+        return this;
+
+    }
+    multiply(value) {
+        this.x *= value;
+        return this;
+    }
+    substract(value) {
+        this.x -= value;
+        return this;
+    }
+    divide(value) {
+        this.x /= value;
+        return this;
+    }
+    square() {
+        this.x = this.x ** 2;
+        return this;
+    }
+    exponent(value) {
+        this.x = this.x ** value;
+        return this;
+    }
+    squareRoot() {
+        this.x = Math.sqrt(this.x);
+        return this;
+    }
+
+    // tambahkan method lain yang perlu
+    result() {
+        console.log(this.x)
+        return this;
+    }
 }
 
-add(value) {
-    this.resultValue += value;
-    return this;
-
-}
-multiply(value){
-    this.resultValue *= value;
-    return this;
-}
-substract(value){
-    this.resultValue -= value;
-    return this;
-}
-divide(value){
-    this.resultValue /= value;
-    return this;
-}
-square(){  
-    this.resultValue = this.resultValue ** 2;
-    return this;
-}
-exponent(value){
-    this.resultValue = this.resultValue ** value; 
-    return this;
-}
-squareRoot(){
-    this.resultValue = Math.sqrt(this.resultValue);
-    return this;
-}
-
-// tambahkan method lain yang perlu
-result(){
-console.log(this.resultValue)
-return this;
-}
-}
-
-export {Calculator, PI}
+export { Calculator, PI }
